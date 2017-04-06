e915d3217861796ea51b8950ee9bd78cd2433445
k22pr <adm@an1.kr>
2017-04-06 10:43:29 +0900

    0.93

--- ext/background.js
+++ ext/background.js
@@ -1,9 +1,11 @@
-
 chrome.webRequest.onBeforeRequest.addListener(function(e){
-	if(e.type == "script" || e.type == "stylesheet") console.log("block : "+e.type);
+	/*
+	if(e.type != "image" && e.type != "main_frame") console.log("block : "+e.type);
 	else console.log("response : "+e.type);
+	*/
 
-	return {cancel : (e.type == "script" || e.type == "stylesheet")}
+	//if(e.url.search("wasabisyrup.com") == -1) return {cancel : false};
+	return {cancel : (e.type != "image" && e.type != "main_frame" && e.type != "other")}
 },
 {urls : ["http://wasabisyrup.com/*"]},
 ["blocking"]);
